e1df31c6549f1bde5b1ace80d7a4c36112825a2b
tryingdaily <zanelemankazana@gmail.com>
2023-04-21 16:53:44 -0700

    Added 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main function
+ * Description: Takess a random number, compares last digit with the number 5
+ * Return: 0
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
