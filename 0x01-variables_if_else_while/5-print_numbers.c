5a85e2a321cb163c1b012cdbec946f04831bc530
tryingdaily <zanelemankazana@gmail.com>
2023-04-21 17:44:02 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - main function
+ * Description: prints all numbers of base 10 starting at 0 and is followed by a new line
+ * Return: 0
+ */
+int main(void)
+{
+	char c = '0';
+
+	while (c <= '9')
+	{
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
