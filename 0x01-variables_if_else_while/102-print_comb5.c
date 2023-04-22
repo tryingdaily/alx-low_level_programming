74d9e9f99968de9e218f3e2e5a58157a4e932721
tryingdaily <zanelemankazana@gmail.com>
2023-04-21 19:36:09 -0700

    Added 102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main function
+ * Description: Write a program that prints all possible different combos of two numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int c1 = 0;
+	int da1; 
+	int db1; 
+
+	int c2;
+	int da2;
+	int db2;
+
+	while (c1 <= 98)
+	{
+		da1 = (c1 / 10 + '0');
+		db1 = (c % 10 + '0');
+		c2 = 0;
+		while (c2 <= 99)
+		{
+			da2 = (c2 / 10 + '0');
+			db2 = (c2 % 10 + '0');
+
+			if (c1 < c2)
+			{
+				putchar(da1);
+				putchar(db1);
+				putchar(' ');
+				putchar(da2);
+				putchar(db2);
+
+				if(c != 98)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+
+			}
+			c2++;
+		}
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
