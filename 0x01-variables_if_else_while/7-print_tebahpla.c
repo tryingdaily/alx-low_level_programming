bee586a312022f0084cf98bbea0a532c5f52c3c4
tryingdaily <zanelemankazana@gmail.com>
2023-04-21 18:03:23 -0700

    Added 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - main funtion
+ * Description: prints all single digit of base 10
+ * Return: 0
+ */
+int main(void)
+{
+	char c = 'z';
+
+	while (c >= 'a')
+	{
+		putchar(c);
+		c--;
+	}
+
+	purchar('\n');
+	return (0);
+}
