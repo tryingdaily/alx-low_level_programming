bf55db73fda68afdaf794f7d4e6432b65fa76d20
tryingdaily <zanelemankazana@gmail.com>
2023-04-21 17:34:31 -0700

    Added 4-print_alphabt.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,10 +3,10 @@
 #include <time.h>
 
 
-/** 
+/**
  * main - main function
  * Description: Takes a random number and prints 5, 0 or 6
- * Return: 0 
+ * Return: 0
  */
 
 int main(void)
@@ -15,16 +15,14 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
 	last = n % 10;
 
 	if (last > 5)
 		printf("Last digit of %i is %i and is greater than 5\n", n, last);
-	else if ( last < 6)
+	else if (last < 6)
 		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, last);
 	else if (last == 0)
 		printf("Last digit is %i is %i and is 0\n", n, last); 
-
 
 	return (0);
 }
---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Description: Get random number and compare with last 5 digit
+ * Return: 0
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if(c != 'e' && c != 'q')
+		{
+			putchar(c);
+		}
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
