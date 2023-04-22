5ed475d29d1a7e3474aba2ffc61709a7b5d8ff71
tryingdaily <zanelemankazana@gmail.com>
2023-04-21 17:03:04 -0700

    Added 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - main function 
+ * Description: prints alphabet in singsong way and is followed by a new line
+ * Return: 0
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+
+	c = 'A';
+
+	while (c <= 'Z')
+	{
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+
+}
