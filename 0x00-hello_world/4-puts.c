36b39bd80451e2e44f90ee9b1e71c7ca88ede445
tryingdaily <zanelemankazana@gmail.com>
2023-04-23 12:13:08 -0700

    Fixed 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /**
- * main : Prints Programming is like building a multilingual puzzle
+ * main -  Prints Programming is like building a multilingual puzzle
+ *
  * Return: 0
  */
 int main(void)
