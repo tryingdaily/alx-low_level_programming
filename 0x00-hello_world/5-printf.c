8a15c2c3215e21891aee1d0282538875d30cf8d0
tryingdaily <zanelemankazana@gmail.com>
2023-04-23 12:05:23 -0700

    Adeed 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main: prints with promper grammer, but the outcome is a piece of art and a new line
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
