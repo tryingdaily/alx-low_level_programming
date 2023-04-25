e962274a3a92bcd71fc44cab31769e311527e2f5
tryingdaily <zanelemankazana@gmail.com>
2023-04-25 12:15:58 -0700

    Added 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,18 +9,15 @@ void print_alphabet_x10(void)
 	char alphabet[26] = "abcdefghijklmnopqrstuvwzyz";
 	int i = 0, j;
 
-	while( i < 10)
+	while (i < 10)
 	{
 		j = 0;
-		
-		while( j < 26)
+		while (j < 26)
 		{
 			_putchar(alphabet[j]);
-			
 			j++;
 		}
-		_putchar('\n'); 
-		
+		_putchar('\n');
 		i++;
 	}
 
