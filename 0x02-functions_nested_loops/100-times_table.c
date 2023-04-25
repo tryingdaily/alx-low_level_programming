a6b986bce0ab94ca083002f38e1d95bdca7a06f3
tryingdaily <zanelemankazana@gmail.com>
2023-04-25 16:46:03 -0700

    Added 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -27,9 +27,9 @@ void print_times_table(int n)
 				if (result >= 100)
 				{
 					_putchar((result / 100) + '0');
-					_putchar((result / 10) % + '0');
+					_putchar((result / 10) % 10 + '0');
 				}
-				else if (result <= 99 && result >=10)
+				else if (result <= 99 && result >= 10)
 				{
 					_putchar((result / 10) + '0');
 
