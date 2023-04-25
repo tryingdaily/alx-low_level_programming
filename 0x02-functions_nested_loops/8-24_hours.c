cdd31b712b3cac42e74015016a691a07b398efaf
tryingdaily <zanelemankazana@gmail.com>
2023-04-25 14:38:01 -0700

    Added 8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *jack_bauer - prints every minute of JB, from 00:00 to 23:59
+ * 
+ * Return: void 
+ */
+void jack_bauer(void)
+{
+	int min, hour;
+	
+	for (hour = 0; hour <= 23; hour++)
+	{
+		for (min = 0; min <= 59; min++)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((min / 10) + '0');
+			_putchar((min % 10) + '0');
+			_putchar(10);
+		}
+	}
+}
