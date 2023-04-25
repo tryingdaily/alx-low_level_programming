3c176f67fa7e210bb31ce8e76dd19259fd524554
tryingdaily <zanelemankazana@gmail.com>
2023-04-25 12:11:54 -0700

    Added 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, followed by a new line
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	char alphabet[26] = "abcdefghijklmnopqrstuvwzyz";
+	int i = 0, j;
+
+	while( i < 10)
+	{
+		j = 0;
+		
+		while( j < 26)
+		{
+			_putchar(alphabet[j]);
+			
+			j++;
+		}
+		putchar('\n'); 
+		
+		i++;
+	}
+
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,6 +8,7 @@
 
 int _putchar(char);
 void print_alphabet(void);
+void print_alphabet_x10(void);
 
 #endif
 
