ea8c56734a2b3d41d594477e271ce7460429d103
tryingdaily <zanelemankazana@gmail.com>
2023-04-25 10:55:11 -0700

    Addded main.h file

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * main - main.h
+ * Description: Header file that contains all the declarations of all functions used in this directory
+ */
+
+int _putchar(char);
+
+#endif
+
+
