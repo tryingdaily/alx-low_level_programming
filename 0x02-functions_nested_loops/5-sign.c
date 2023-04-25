953f91595b7ab34ea50970816f5dc01b1543606f
tryingdaily <zanelemankazana@gmail.com>
2023-04-25 13:01:56 -0700

    Added 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign -  prints the sign of a number
+ *
+ * @n: input value 
+ *
+ * Return: 1 and print + if n > 0, 0 prints 0 if n = 0 and -1 and print - if n < 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+	       return (-1);
+	}	       
+
+}
