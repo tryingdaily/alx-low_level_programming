393cba699e50e74504cf1a6dcba323d6c3f27ef8
tryingdaily <zanelemankazana@gmail.com>
2023-05-04 16:28:48 -0700

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,30 +1,29 @@
+#include "main.h"
+
 /**
- * rot13 - Encodes a string using ROT13 cipher
- * @str: Pointer to the string to be encoded
- *
- * Return: Pointer to the encoded string
+ * *rot13 - encodes a string using rot13.
+ * @s: int type array pointer
+ * Return: encoded
  */
-char *rot13(char *str)
+
+char *rot13(char *s)
 {
-    int i, j;
-    char c;
+	int i, ii;
+	char input[] =  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char output[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-    for (i = 0; str[i] != '\0'; i++) {
-        if (str[i] >= 'a' && str[i] <= 'z') {
-            // Rotate lowercase letters by 13 positions
-            c = str[i] + 13;
-            if (c > 'z')
-                c -= 26;
-            str[i] = c;
-        } else if (str[i] >= 'A' && str[i] <= 'Z') {
-            // Rotate uppercase letters by 13 positions
-            c = str[i] + 13;
-            if (c > 'Z')
-                c -= 26;
-            str[i] = c;
-        }
-    }
-
-    return str;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (ii = 0; ii < 54; ii++)
+		{
+			if (((s[i] <= 'z' && s[i] >= 'a') || (s[i] <= 'Z' && s[i] >= 'A'))
+			&& s[i] == input[ii])
+			{
+				s[i] = output[ii];
+				break;
+			}
+		}
+	}
+	return (s);
 }
 
